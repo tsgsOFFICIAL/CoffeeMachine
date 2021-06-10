ed8c75cb64da59a57771f734573d19ffb837286a
Marcus Mortensen Jensen <marc7075@zbc.dk>
2021-06-10 12:38:35 +0200

    SOLID

--- Brewer.cs
+++ Brewer.cs
@@ -4,6 +4,14 @@ namespace CoffeeMachine
 {
     public class Brewer
     {
-
+        public Container LiquidContainer { get; private set; }
+        public Container BeanContainer { get; private set; }
+        public Filter MachineFilter { get; private set; }
+        public Brewer(Container liquidContainer, Container beanContainer, Filter machineFilter)
+        {
+            LiquidContainer = liquidContainer;
+            BeanContainer = beanContainer;
+            MachineFilter = machineFilter;
+        }
     }
 }
